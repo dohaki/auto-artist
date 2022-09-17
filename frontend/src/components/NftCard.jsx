cc5b69e289ca2171bfaedcbcdd4f391098ae98a1
Dong-Ha Kim <dongha.kim210@gmail.com>
2022-09-17 23:27:03 +0200

    fix: url

--- frontend/src/components/NftCard.jsx
+++ frontend/src/components/NftCard.jsx
@@ -3,7 +3,12 @@ import { Box, Image, Text } from "@chakra-ui/react";
 export function NftCard({ tokenUri, prompt }) {
   return (
     <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
-      <Image width={400} height={400} src={tokenUriToPinataUrl(tokenUri)} />
+      <Image
+        width={400}
+        height={400}
+        src={tokenUriToPinataUrl(tokenUri)}
+        alt={tokenUri}
+      />
       <Box p="6">
         <Box mt="1" lineHeight="tight">
           <Text fontSize="sm">{prompt}</Text>
