ecc40a6aa539bac40c87beaa30c538e2a147a2ba
Dong-Ha Kim <dongha.kim210@gmail.com>
2022-09-18 10:47:36 +0200

    fix: finalize flow

--- frontend/src/components/Finalize.jsx
+++ frontend/src/components/Finalize.jsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import { Button } from "@chakra-ui/react";
 
 import { useFinalize } from "../hooks/useFinalize";
+import { useTokenInfo } from "../hooks/useTokenInfo";
 
 export function Finalize({ tokenId }) {
   const finalize = useFinalize(tokenId);
+  const [txHash, setTxHash] = useState();
+
+  const tokenInfo = useTokenInfo(tokenId);
 
   return (
     <Button
       colorScheme="purple"
-      disabled={!finalize.write}
-      onClick={() => finalize?.write()}
+      disabled={!finalize.write || txHash}
+      onClick={() => {
+        finalize
+          ?.writeAsync()
+          .then((data) => {
+            setTxHash(data.hash);
+            return data.wait();
+          })
+          .then((receipt) => {
+            setTxHash("");
+            tokenInfo.refetch();
+          });
+      }}
       isLoading={finalize.isLoading}
     >
       Finalize
